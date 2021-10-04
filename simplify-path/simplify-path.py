f972b6f924903477893c176ac5acd104e49f72be
Md. Jahid Hasan Mridha <60375584+zahidshaon@users.noreply.github.com>
2021-10-04 22:53:57 +0600

    Time: 61 ms (7.84%), Space: 14.4 MB (43.51%) - LeetHub

---
+++ simplify-path/simplify-path.py
@@ -0,0 +1,23 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        i = 0
+        ans = ''
+        while i < len(path):
+            end = i + 1
+            while end < len(path) and path[end] != '/':
+                end += 1
+            sub = path[i + 1:end]
+            if len(sub) > 0:
+                if sub == '..':
+                    if stack != []:
+                        stack.pop()
+                elif sub != '.':
+                    stack.append(sub)
+            i = end
+        if stack == []:
+            return '/'
+        for i in stack:
+            ans += '/' + i
+        return ans
+        
